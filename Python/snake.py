b1f537fb1d7197f5b32f9a9dcd762dc92d8c8176
Santicano73 <57371234+Santicano73@users.noreply.github.com>
2021-03-19 02:58:05 -0600

    Changes in PacMan and Snake
    PacMan Map more interesting and Colors in Snake changed

--- Python/snake.py
+++ Python/snake.py
@@ -32,7 +32,7 @@ def move():
     head.move(aim)
 
     if not inside(head) or head in snake:
-        square(head.x, head.y, 9, 'red')
+        square(head.x, head.y, 9, 'black')
         update()
         return
 
@@ -48,9 +48,9 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, 'green')
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, 'red')
     update()
     ontimer(move, 100)
 
